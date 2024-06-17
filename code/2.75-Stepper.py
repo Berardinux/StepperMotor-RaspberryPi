1b9d71cef9945108c748e09352d547691241cbe4
Berardinux <berardinux@gmail.com>
2024-06-17 18:06:30 -0400

    Jun 17, 18:06

--- code/2.75-Stepper.py
+++ code/2.75-Stepper.py
@@ -41,8 +41,8 @@ def Ramp(prev_switch_state):
   for i in range(PPS, 0, -1):
     pi.set_PWM_frequency(STEP, i)
     pi.write(DIR, pi.read(prev_switch_state))
-    print("Previous: " + prev_switch_state)
-    print("Current: " + current_switch_state)
+    print("Previous: ", prev_switch_state)
+    print("Current: ", current_switch_state)
     sleep(.00005)
     if i < PPS/2:
       -i
