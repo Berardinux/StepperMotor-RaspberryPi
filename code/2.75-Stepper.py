2f1cc1a501883dfdabf470ff00ccbb526651e86b
Berardinux <berardinux@gmail.com>
2024-06-17 18:51:29 -0400

    Jun 17, 18:51

--- code/2.75-Stepper.py
+++ code/2.75-Stepper.py
@@ -2,8 +2,9 @@ from time import sleep
 import pigpio
 import RPi.GPIO as GPIO
 
+# Define constants
 M0 = 14
-M1 = 15 # Microstepping pins
+M1 = 15  # Microstepping pins
 M2 = 18
 
 DIR = 20
@@ -11,60 +12,68 @@ STEP = 21
 SWITCH = 16
 PPS = 1000
 SUB = -4
-ADD = +4
+ADD = 4
 
+# Initialize pigpio and GPIO
 pi = pigpio.pi()
 
 pi.set_mode(SWITCH, pigpio.INPUT)
 pi.set_pull_up_down(SWITCH, pigpio.PUD_UP)
 
-pi.set_PWM_dutycycle(STEP, 128) # 50% On 50% Off 
-pi.set_PWM_frequency(STEP, PPS) # 1000 pulses per second 
+pi.set_PWM_dutycycle(STEP, 128)  # 50% On 50% Off
+pi.set_PWM_frequency(STEP, PPS)  # 1000 pulses per second
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(M0, GPIO.OUT)
-GPIO.setup(M1, GPIO.OUT) # Microstepping pins
-GPIO.setup(M2, GPIO.OUT) 
+GPIO.setup(M1, GPIO.OUT)  # Microstepping pins
+GPIO.setup(M2, GPIO.OUT)
 
+# Set microstepping mode
 # Full Step          (000)
 # Half Step          (100)
 # Quarter Step       (010)
 # Eigth Step         (110)
 # Sixteenth Step     (001)
 # Thirty-Second Step (101)
-
 GPIO.output(M0, GPIO.HIGH)
-GPIO.output(M1, GPIO.LOW) # Microstepping output
+GPIO.output(M1, GPIO.LOW)  # Microstepping output
 GPIO.output(M2, GPIO.LOW)
 
 def Ramp(prev_switch_state):
-  for i in range(PPS/1.5, 100, SUB):
-    pi.set_PWM_frequency(STEP, i)
-    pi.write(DIR, prev_switch_state)
-    sleep(.0000005)
-  for i in range(100, PPS/1.5, ADD):
-    pi.set_PWM_frequency(STEP, i)
-    pi.write(DIR, current_switch_state)
-    sleep(.0000005)
-  print("Previous: ", prev_switch_state)
-  print("Current: ", current_switch_state)
-  print("Ramp")
+    start = int(PPS / 1.5)  # Calculate starting point and convert to integer
+    end = 100
+
+    # Decrement loop
+    for i in range(start, end, SUB):
+        pi.set_PWM_frequency(STEP, i)
+        pi.write(DIR, prev_switch_state)
+        sleep(0.0000005)
+
+    # Increment loop
+    for i in range(end, start, ADD):
+        pi.set_PWM_frequency(STEP, i)
+        pi.write(DIR, current_switch_state)
+        sleep(0.0000005)
+
+    print("Previous: ", prev_switch_state)
+    print("Current: ", current_switch_state)
+    print("Ramp")
 
 try:
-  prev_switch_state = pi.read(SWITCH)
-  while True:
-    current_switch_state = pi.read(SWITCH)
-    if prev_switch_state != current_switch_state:
-      Ramp(prev_switch_state)
-    pi.write(DIR, pi.read(SWITCH))
-    prev_switch_state = current_switch_state
-    sleep(.1)
-    
-    print("In While Loop Current: ", pi.read(SWITCH))
+    prev_switch_state = pi.read(SWITCH)
+    while True:
+        current_switch_state = pi.read(SWITCH)
+        if prev_switch_state != current_switch_state:
+            Ramp(prev_switch_state)
+        pi.write(DIR, pi.read(SWITCH))
+        prev_switch_state = current_switch_state
+        sleep(0.1)
+
+        print("In While Loop Current: ", pi.read(SWITCH))
 
 except KeyboardInterrupt:
-  print ("\nCtrl -C pressed.")
+    print("\nCtrl -C pressed.")
 finally:
-  pi.set_PWM_dutycycle(STEP, 0)
-  pi.stop()
-  GPIO.cleanup()
+    pi.set_PWM_dutycycle(STEP, 0)
+    pi.stop()
+    GPIO.cleanup()
